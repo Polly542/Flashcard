60f424a1a72e2cd31aac32babde3a893bc66db3b
Polly542 <146094570+Polly542@users.noreply.github.com>
2024-09-23 08:23:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Skate',
+    'O que é Python?',
+    'Grupos de surfistas instalaram rodas e eixos nas pranchas para simular o surfe nas calçadas'
+)
+
+criaCartao(
+    'Skate',
+    'Onde foi criado o skate?',
+    'Estados Unidos - California'
+)
+
+criaCartao(
+    'Skate',
+    'Qual a primeira olimpiadas que o skate participou?',
+    'Olimpiadas de Tóquio 2020'
+)
