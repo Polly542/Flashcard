d745e396b5a7a3c73d0e3289ae56b44b2463e71d
Polly542 <146094570+Polly542@users.noreply.github.com>
2024-10-07 08:41:35 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,15 @@ criaCartao(
     'Qual a primeira olimpiadas que o skate participou?',
     'Olimpiadas de Tóquio 2020'
 )
+
+criaCartao(
+    'Skate',
+    'Qual a diferença entre skate street e skate park?',
+    'Skate street: os atletas têm à disposição escadas e corrimãos para executar suas manobras. Skate park: é realizado em uma pista arredondada, potencializando a velocidade dos atletas'
+)
+
+criaCartao(
+    'Skate',
+    'Qual o skatista mais famoso do mundo?',
+    'Anthony Frank Hawk, mais conhecido como Tony Hawk, é um skatista norte-americano de base goofy de grande destaque na modalidade vertical do esporte radical, um dos fundadores da marca Birdhouse Skateboards e o primeiro atleta a realizar a manobra "900"'
+)
